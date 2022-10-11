acd4b366fe1d422d1d1529560b3972a4b3a42e78
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-10-11 11:46:39 +0100

    change Persistence mapping to Extbase configuration file, fix label

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    Qc\QcComments\Domain\Model\Comment::class => [
+        'tableName' => 'tx_qccomments_domain_model_comment',
+        'date_houre' => 'dateHoure',
+        'url_orig' => 'urlOrig',
+        'uid_orig' => 'uidOrig',
+        'uid_perms_group' => 'uidPermsGroup'
+    ],
+];
