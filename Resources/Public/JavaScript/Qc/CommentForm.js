78cd1b2bdc7f205788a2c344027e1781befd7607
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-10-14 17:28:13 +0100

    code cleaning

--- Resources/Public/JavaScript/Qc/CommentForm.js
+++ Resources/Public/JavaScript/Qc/CommentForm.js
@@ -18,7 +18,6 @@ $(document).ready(function(){
         textareaElement = document.getElementById('comment-textarea');
         let initialLimit = Number(maxCharacters);
 
-        var t = limitLabel.innerHTML
         maxLabel = document.getElementById('maxLabel').getAttribute('data-tr-label')
         charLabel = document.getElementById('charLabel').getAttribute('data-tr-label')
 
