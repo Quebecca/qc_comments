976f00d8c33997e36ac5685c8b3465deaabf8d57
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2022-10-17 15:49:31 +0100

    Js code refactoring

--- Resources/Public/JavaScript/Pgu/CommentForm.js
+++ Resources/Public/JavaScript/Pgu/CommentForm.js
@@ -1,5 +1,6 @@
 if (document.getElementById('commentForm') !== null) {
-    let siteKey = document.getElementById('sitekey').getAttribute('data-tr-label') !== null ? document.getElementById('sitekey').getAttribute('data-tr-label') : '';
+    let siteKeyElement = document.getElementById('sitekey');
+    let siteKey = siteKeyElement.getAttribute('data-tr-label') ?? '';
     window.addEventListener("DOMContentLoaded", function () {
         $('#commentForm').submit(function (e) {
             grecaptcha.ready(function (token) {
@@ -12,6 +13,11 @@ if (document.getElementById('commentForm') !== null) {
 
     function onCompleted() {
         $('form .powermail_submit').prop("disabled", true);
+        document.getElementById('comment-section').className = 'form-group d-block';
+        document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
+        //$( "#usefulN" ).prop( "checked", false);
+        //$( "#usefulY" ).prop( "checked", true);
+        //$("#comment-textarea").val("");
         setTimeout(function enable() {
             submitAmount = 0;
             $('form .powermail_submit').prop("disabled", false);
@@ -19,8 +25,8 @@ if (document.getElementById('commentForm') !== null) {
         $('#commentForm').trigger('submit', [true]);
     }
 
-    let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') != null
-        ? document.getElementById('maxCharacters').getAttribute('data-tr-label') : '';
+    let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') ?? '';
+
     let limitLabel = document.getElementById('limitLabel');
     textareaElement = document.getElementById('comment-textarea');
     let initialLimit = Number(maxCharacters);
@@ -32,15 +38,15 @@ if (document.getElementById('commentForm') !== null) {
     textareaElement.setAttribute("maxlength", maxCharacters);
     textareaElement.addEventListener("keydown", (event) => {
         var length = textareaElement.value.length
-        var x = initialLimit === length ? 0
+        var limit = initialLimit === length ? 0
             : (length === 0 ? initialLimit
                 : (initialLimit - length));
-        if (event.code !== 'Backspace' && x >= 1) {
-            limitLabel.innerText = maxLabel + (x-1) + charLabel;
+        if (event.code !== 'Backspace' && limit >= 1) {
+            limitLabel.innerText = maxLabel + (limit-1) + charLabel;
         }
         else{
-            if(x !== initialLimit && event.code === 'Backspace'){
-                limitLabel.innerText = maxLabel + (x+1) + charLabel;
+            if(limit !== initialLimit && event.code === 'Backspace'){
+                limitLabel.innerText = maxLabel + (limit+1) + charLabel;
             }
             else if(initialLimit === length){
                 limitLabel.innerText = maxLabel + "0"+ charLabel;
