e99f3d77244b5e23b7f4ddd37aa9934e504c8011
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2023-02-16 16:27:23 +0100

    activer le recaptcha

--- Resources/Public/JavaScript/Pgu/CommentForm.js
+++ Resources/Public/JavaScript/Pgu/CommentForm.js
@@ -1,25 +1,21 @@
-// Form validation
-// comment length > 2 and < 500
-// comment not empty
-// if true disable submit button
-// Il faut vérifier que usefull or non is checked
-
 
 if (document.getElementById('commentForm') !== null) {
+
     let siteKeyElement = document.getElementById('sitekey');
     let siteKey = siteKeyElement.getAttribute('data-tr-label') ?? '';
     let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') ?? '';
     let minCharacters = document.getElementById('minCharacters').getAttribute('data-tr-label') ?? '';
 
-    function commentValidation() {
-        let textareaElement = $('#comment-textarea');
-        var commentLength = textareaElement.val().length;
-        let validComment = ( minCharacters <= commentLength && commentLength <= maxCharacters ) || commentLength === 0;
-        $('#submitButton').attr('disabled', !validComment)
-        $('#error-message-to-short').toggleClass('d-none', validComment)
-        $(textareaElement).toggleClass('error-textarea',!validComment)
-        return validComment;
-    }
+    // Afficher le text-area quand on sélectionne "Oui" ou "Non"
+    [
+        document.getElementById('usefulN'),
+        document.getElementById('usefulY')
+    ].map(function (element) {
+        element.addEventListener("change", function () {
+            document.getElementById('comment-section').className = 'form-group d-block';
+            document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
+        });
+    });
 
     window.addEventListener("DOMContentLoaded", function () {
 
@@ -42,17 +38,25 @@ if (document.getElementById('commentForm') !== null) {
         })
     });
 
-
-    // Recaptcha
     function onCompleted() {
-        $('form .powermail_submit').prop("disabled", true);
+        $('submitButton').prop("disabled", true);
         document.getElementById('comment-section').className = 'form-group d-block';
         document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
         setTimeout(function enable() {
             submitAmount = 0;
-            $('form .powermail_submit').prop("disabled", false);
+            $('#submitButton').prop("disabled", false);
         }, 2000);
         $('#commentForm').trigger('submit', [true]);
+    }
+
+    function commentValidation() {
+        let textareaElement = $('#comment-textarea');
+        var commentLength = textareaElement.val().length;
+        let validComment = ( minCharacters <= commentLength && commentLength <= maxCharacters ) || commentLength === 0;
+        $('#submitButton').attr('disabled', !validComment)
+        $('#error-message-to-short').toggleClass('d-none', validComment)
+        $(textareaElement).toggleClass('error-textarea',!validComment)
+        return validComment;
     }
 
     function checkCommentLength(){
@@ -61,16 +65,4 @@ if (document.getElementById('commentForm') !== null) {
         document.getElementById('limitLabel').innerHTML = maxLabel + (maxCharacters -  $('#comment-textarea').val().length) + charLabel
 
     }
-
-    // Afficher le text-area quand on selectionne "Oui" ou "Non"
-    [
-        document.getElementById('usefulN'),
-        document.getElementById('usefulY')
-    ].map(function (element) {
-        element.addEventListener("change", function () {
-            document.getElementById('comment-section').className = 'form-group d-block';
-            document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
-        });
-    });
-
 }
