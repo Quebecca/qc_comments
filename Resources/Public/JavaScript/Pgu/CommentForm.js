74b0475f8e0bd22231890fca451902b05d02cf05
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2023-03-28 17:16:49 +0000

    Code enhacement, add dyanamic tt_content

--- Resources/Public/JavaScript/Pgu/CommentForm.js
+++ Resources/Public/JavaScript/Pgu/CommentForm.js
@@ -1,108 +1,98 @@
-$(document).ready(function(){
-    if (document.getElementById('commentForm') !== null) {
-        let siteKey = document.getElementById('sitekey').getAttribute('data-tr-label') ?? '';
-        let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') ?? '';
-        let minCharacters = document.getElementById('minCharacters').getAttribute('data-tr-label') ?? '';
-        let skipRecaptcha = document.getElementById('skipRecaptcha').getAttribute('data-tr-label') ?? '';
-        // Afficher le text-area quand on sélectionne "Oui" ou "Non"
-        [
-            document.getElementById('usefulN'),
-            document.getElementById('usefulY')
-        ].map(function (element) {
-            element.addEventListener("change", function () {
-                document.getElementById('comment-section').className = 'form-group d-block';
-                document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
-            });
+if (document.getElementById('commentForm') !== null) {
+    let siteKey = document.getElementById('sitekey').getAttribute('data-tr-label') ?? '';
+    let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') ?? '';
+    let minCharacters = document.getElementById('minCharacters').getAttribute('data-tr-label') ?? '';
+    let skipRecaptcha = document.getElementById('skipRecaptcha').getAttribute('data-tr-label') ?? '';
+    // Afficher le text-area quand on sélectionne "Oui" ou "Non"
+    [
+        document.getElementById('usefulN'),
+        document.getElementById('usefulY')
+    ].map(function (element) {
+        element.addEventListener("change", function () {
+            document.getElementById('comment-section').className = 'form-group d-block';
+            document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
         });
+    });
 
-        var submitAmount = 0;
-        var isRecaptchaLoaded = false;
-        $('#commentForm :input').on('click',function (){
-            if(!isRecaptchaLoaded && skipRecaptcha === '1'){
-                var lang = $('#lang').attr('data-tr-label')
-                // trigger loading api.js (recaptcha.js) script
-                var head = document.getElementsByTagName('head')[0];
-                var script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.src = 'https://www.google.com/recaptcha/api.js?render='+siteKey+'&hl='+lang;
-                head.appendChild(script);
-                isRecaptchaLoaded = true
-            }
-        })
+    var submitAmount = 0;
+    var isRecaptchaLoaded = false;
+    $('#commentForm :input').on('click',function (){
+        if(!isRecaptchaLoaded && skipRecaptcha === '1'){
+            var lang = $('#lang').attr('data-tr-label')
+            // trigger loading api.js (recaptcha.js) script
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://www.google.com/recaptcha/api.js?render='+siteKey+'&hl='+lang;
+            head.appendChild(script);
+            isRecaptchaLoaded = true
+        }
+    })
 
+    $('#comment-textarea').on('keyup', function () {
+        checkCommentLength();
+    });
+    $('#commentForm').submit(function (event) {
+
+        if (skipRecaptcha === '0' && commentValidation()) {
+            $('#submitButton').prop("disabled", true);
+            return true;
+        }
+        if(!commentValidation()){
+            event.preventDefault()
+        }
         $('#comment-textarea').on('keyup', function () {
-            checkCommentLength();
+            commentValidation();
         });
-        $('#commentForm').submit(function (event) {
 
-            if (skipRecaptcha === '0' && commentValidation()) {
-                $('#submitButton').prop("disabled", true);
-                return true;
-            }
-            if(!commentValidation()){
-                event.preventDefault()
-            }
-            $('#comment-textarea').on('keyup', function () {
-                commentValidation();
-            });
-
-            if (typeof grecaptcha == 'object') {
-                if (!grecaptcha.getResponse()) {
-                    event.preventDefault();
-                    grecaptcha.execute();
-                }
-            } else {
+        if (typeof grecaptcha == 'object') {
+            if (!grecaptcha.getResponse()) {
                 event.preventDefault();
+                grecaptcha.execute();
             }
-            $('#submitButton').prop("disabled", true);
-
-        });
-
-        $('#commentForm #submitButton').mousedown(function (event) {
-            var field = this;
-            if (submitAmount === 0) {
-                submitAmount++;
-                field.disabled = true;
-                setTimeout(function enable() {
-                    submitAmount = 0;
-                    field.disabled = false;
-                }, 6000);
-                $('#commentForm').trigger('submit');
-            }
-        });
-
-
-        function onCompleted() {
-            $('#submitButton').prop("disabled", true);
-            // document.getElementById('comment-section').className = 'form-group d-block';
-            // document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
-            /*     setTimeout(function enable() {
-                      submitAmount = 0;
-                      $('#submitButton').prop("disabled", false);
-                  }, 2000);*/
-            $('#commentForm').trigger('submit', [true]);
+        } else {
+            event.preventDefault();
         }
+        $('#submitButton').prop("disabled", true);
 
-        function commentValidation() {
-            let textareaElement = $('#comment-textarea');
-            var commentLength = textareaElement.val().length;
-            let validComment = ( minCharacters <= commentLength && commentLength <= maxCharacters ) || commentLength === 0;
-            $('#submitButton').attr('disabled', !validComment)
-            $('#error-message-too-short').toggleClass('d-none', validComment)
-            $(textareaElement).toggleClass('error-textarea',!validComment)
-            return validComment;
-        }
+    });
 
-        function checkCommentLength(){
-            maxLabel = document.getElementById('maxLabel').getAttribute('data-tr-label')
-            charLabel = document.getElementById('charLabel').getAttribute('data-tr-label')
-            var currentLength = maxCharacters -  $('#comment-textarea').val().length
-            if(currentLength >= 0){
-                document.getElementById('limitLabel').innerHTML = maxLabel + currentLength + charLabel
-            }
-            /*if(currentLength <= 0){
-                $('#comment-textarea').attr("maxlength",0);
-            }*/
+    $('#commentForm #submitButton').mousedown(function (event) {
+        var field = this;
+        if (submitAmount === 0) {
+            submitAmount++;
+            field.disabled = true;
+            setTimeout(function enable() {
+                submitAmount = 0;
+                field.disabled = false;
+            }, 6000);
+            $('#commentForm').trigger('submit');
         }
+    });
+
+    function commentValidation() {
+        let textareaElement = $('#comment-textarea');
+        var commentLength = textareaElement.val().length;
+        let validComment = ( minCharacters <= commentLength && commentLength <= maxCharacters ) || commentLength === 0;
+        $('#submitButton').attr('disabled', !validComment)
+        $('#error-message-too-short').toggleClass('d-none', validComment)
+        $(textareaElement).toggleClass('error-textarea',!validComment)
+        return validComment;
     }
-})
+
+    function checkCommentLength(){
+        maxLabel = document.getElementById('maxLabel').getAttribute('data-tr-label')
+        charLabel = document.getElementById('charLabel').getAttribute('data-tr-label')
+        var currentLength = maxCharacters -  $('#comment-textarea').val().length
+        if(currentLength >= 0){
+            document.getElementById('limitLabel').innerHTML = maxLabel + currentLength + charLabel
+        }
+        /*if(currentLength <= 0){
+            $('#comment-textarea').attr("maxlength",0);
+        }*/
+    }
+    function onCompleted() {
+        $('#submitButton').prop("disabled", true);
+        $('#commentForm').trigger('submit', [true]);
+    }
+}
