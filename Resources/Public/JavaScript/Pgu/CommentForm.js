26b246370b1f77ca19288f3ed68bb5233e7c9e4f
Tgiti Abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2023-03-31 16:39:22 +0000

    Code enhancement, bug fixes

--- Resources/Public/JavaScript/Pgu/CommentForm.js
+++ Resources/Public/JavaScript/Pgu/CommentForm.js
@@ -1,98 +1,101 @@
-if (document.getElementById('commentForm') !== null) {
-    let siteKey = document.getElementById('sitekey').getAttribute('data-tr-label') ?? '';
-    let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') ?? '';
-    let minCharacters = document.getElementById('minCharacters').getAttribute('data-tr-label') ?? '';
-    let skipRecaptcha = document.getElementById('skipRecaptcha').getAttribute('data-tr-label') ?? '';
-    // Afficher le text-area quand on sélectionne "Oui" ou "Non"
-    [
-        document.getElementById('usefulN'),
-        document.getElementById('usefulY')
-    ].map(function (element) {
-        element.addEventListener("change", function () {
-            document.getElementById('comment-section').className = 'form-group d-block';
-            document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
-        });
-    });
-
-    var submitAmount = 0;
-    var isRecaptchaLoaded = false;
-    $('#commentForm :input').on('click',function (){
-        if(!isRecaptchaLoaded && skipRecaptcha === '1'){
-            var lang = $('#lang').attr('data-tr-label')
-            // trigger loading api.js (recaptcha.js) script
-            var head = document.getElementsByTagName('head')[0];
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://www.google.com/recaptcha/api.js?render='+siteKey+'&hl='+lang;
-            head.appendChild(script);
-            isRecaptchaLoaded = true
-        }
-    })
-
-    $('#comment-textarea').on('keyup', function () {
-        checkCommentLength();
-    });
-    $('#commentForm').submit(function (event) {
-
-        if (skipRecaptcha === '0' && commentValidation()) {
-            $('#submitButton').prop("disabled", true);
-            return true;
-        }
-        if(!commentValidation()){
-            event.preventDefault()
-        }
-        $('#comment-textarea').on('keyup', function () {
-            commentValidation();
+$( document ).ready(function() {
+    if (document.getElementById('commentForm') !== null) {
+        let siteKey = document.getElementById('sitekey').getAttribute('data-tr-label') ?? '';
+        let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') ?? '';
+        let minCharacters = document.getElementById('minCharacters').getAttribute('data-tr-label') ?? '';
+        let skipRecaptcha = document.getElementById('skipRecaptcha').getAttribute('data-tr-label') ?? '';
+        // Afficher le text-area quand on sélectionne "Oui" ou "Non"
+        [
+            document.getElementById('usefulN'),
+            document.getElementById('usefulY')
+        ].map(function (element) {
+            element.addEventListener("change", function () {
+                document.getElementById('comment-section').className = 'form-group d-block';
+                document.getElementById('questionsLink-section').className = 'col-12 col-lg-4 flex-wrap flex-lg-wrap-reverse sansBorder d-block';
+            });
         });
 
-        if (typeof grecaptcha == 'object') {
-            if (!grecaptcha.getResponse()) {
-                event.preventDefault();
-                grecaptcha.execute();
+        var submitAmount = 0;
+        var isRecaptchaLoaded = false;
+        $('#commentForm :input').on('click',function (){
+            if(!isRecaptchaLoaded && skipRecaptcha === '1'){
+                var lang = $('#lang').attr('data-tr-label')
+                // trigger loading api.js (recaptcha.js) script
+                var head = document.getElementsByTagName('head')[0];
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = 'https://www.google.com/recaptcha/api.js?render='+siteKey+'&hl='+lang;
+                head.appendChild(script);
+                isRecaptchaLoaded = true
             }
-        } else {
-            event.preventDefault();
+        })
+
+        $('#comment-textarea').on('keyup', function () {
+            checkCommentLength();
+        });
+        $('#commentForm').submit(function (event) {
+
+            if (skipRecaptcha === '0' && commentValidation()) {
+                $('#submitButton').prop("disabled", true);
+                return true;
+            }
+            if(!commentValidation()){
+                event.preventDefault()
+            }
+            $('#comment-textarea').on('keyup', function () {
+                commentValidation();
+            });
+
+            if (typeof grecaptcha == 'object') {
+                if (!grecaptcha.getResponse()) {
+                    event.preventDefault();
+                    grecaptcha.execute();
+                }
+            } else {
+                event.preventDefault();
+            }
+            $('#submitButton').prop("disabled", true);
+
+        });
+
+        $('#commentForm #submitButton').mousedown(function (event) {
+            var field = this;
+            if (submitAmount === 0) {
+                submitAmount++;
+                field.disabled = true;
+                setTimeout(function enable() {
+                    submitAmount = 0;
+                    field.disabled = false;
+                }, 6000);
+                $('#commentForm').trigger('submit');
+            }
+        });
+
+        function commentValidation() {
+            let textareaElement = $('#comment-textarea');
+            var commentLength = textareaElement.val().length;
+            let validComment = ( minCharacters <= commentLength && commentLength <= maxCharacters ) || commentLength === 0;
+            $('#submitButton').attr('disabled', !validComment)
+            $('#error-message-too-short').toggleClass('d-none', validComment)
+            $(textareaElement).toggleClass('error-textarea',!validComment)
+            return validComment;
         }
-        $('#submitButton').prop("disabled", true);
 
-    });
-
-    $('#commentForm #submitButton').mousedown(function (event) {
-        var field = this;
-        if (submitAmount === 0) {
-            submitAmount++;
-            field.disabled = true;
-            setTimeout(function enable() {
-                submitAmount = 0;
-                field.disabled = false;
-            }, 6000);
-            $('#commentForm').trigger('submit');
+        function checkCommentLength(){
+            maxLabel = document.getElementById('maxLabel').getAttribute('data-tr-label')
+            charLabel = document.getElementById('charLabel').getAttribute('data-tr-label')
+            var currentLength = maxCharacters -  $('#comment-textarea').val().length
+            if(currentLength >= 0){
+                document.getElementById('limitLabel').innerHTML = maxLabel + currentLength + charLabel
+            }
+            /*if(currentLength <= 0){
+                $('#comment-textarea').attr("maxlength",0);
+            }*/
         }
-    });
 
-    function commentValidation() {
-        let textareaElement = $('#comment-textarea');
-        var commentLength = textareaElement.val().length;
-        let validComment = ( minCharacters <= commentLength && commentLength <= maxCharacters ) || commentLength === 0;
-        $('#submitButton').attr('disabled', !validComment)
-        $('#error-message-too-short').toggleClass('d-none', validComment)
-        $(textareaElement).toggleClass('error-textarea',!validComment)
-        return validComment;
     }
-
-    function checkCommentLength(){
-        maxLabel = document.getElementById('maxLabel').getAttribute('data-tr-label')
-        charLabel = document.getElementById('charLabel').getAttribute('data-tr-label')
-        var currentLength = maxCharacters -  $('#comment-textarea').val().length
-        if(currentLength >= 0){
-            document.getElementById('limitLabel').innerHTML = maxLabel + currentLength + charLabel
-        }
-        /*if(currentLength <= 0){
-            $('#comment-textarea').attr("maxlength",0);
-        }*/
-    }
-    function onCompleted() {
-        $('#submitButton').prop("disabled", true);
-        $('#commentForm').trigger('submit', [true]);
-    }
+});
+function onCompleted() {
+    $('#submitButton').prop("disabled", true);
+    $('#commentForm').trigger('submit', [true]);
 }
