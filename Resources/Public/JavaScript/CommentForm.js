61582faee6c3fbe0662c7643c9e1e4d615ded82d
abderrazak <abderrazak.tigiti@mce.gouv.qc.ca>
2023-09-19 12:18:50 +0100

    Support for recaptcah V3

--- Resources/Public/JavaScript/CommentForm.js
+++ Resources/Public/JavaScript/CommentForm.js
@@ -1,8 +1,9 @@
-$( document ).ready(function() {
+var submitAmount = 0;
 
+$( document ).ready(function() {
     let isFormSubmittedElement = document.getElementById('isFormSubmitted');
     if(isFormSubmittedElement !== null){
-        let submitted = isFormSubmitted.getAttribute('data-tr-label')
+        let submitted = isFormSubmittedElement.getAttribute('data-tr-label')
         if(['true', 'false'].includes(submitted)){
             let anchor = document.getElementById('comments-form-section');
             anchor.scrollIntoView()
@@ -10,11 +11,10 @@ $( document ).ready(function() {
     }
     if (document.getElementById('commentForm') !== null) {
 
-
-        let siteKey = document.getElementById('sitekey').getAttribute('data-tr-label') ?? '';
         let maxCharacters = document.getElementById('maxCharacters').getAttribute('data-tr-label') ?? '';
         let minCharacters = document.getElementById('minCharacters').getAttribute('data-tr-label') ?? '';
         let skipRecaptcha = document.getElementById('skipRecaptcha').getAttribute('data-tr-label') ?? '';
+
         [
             document.getElementById('usefulN'),
             document.getElementById('usefulY')
@@ -25,66 +25,51 @@ $( document ).ready(function() {
             });
         });
 
-        var submitAmount = 0;
-        var isRecaptchaLoaded = false;
-        $('#commentForm :input').on('click',function (){
-            if(!isRecaptchaLoaded && skipRecaptcha === true){
-                var lang = $('#lang').attr('data-tr-label')
-                var head = document.getElementsByTagName('head')[0];
-                var script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.src = 'https://www.google.com/recaptcha/api.js?render='+siteKey+'&hl='+lang;
-                script.defer = true
-                script.async = true
-                head.appendChild(script);
-                isRecaptchaLoaded = true
-            }
-        })
 
         $('#comment-textarea').on('keyup', function () {
             checkCommentLength();
         });
-        $('#commentForm').submit(function (event) {
 
+        $('#submitButton').click(function (event) {
             if (!skipRecaptcha && commentValidation()) {
                 $('#submitButton').prop("disabled", true);
+                $('#commentForm').trigger('submit', [true]);
+
                 return true;
             }
-            if(!commentValidation()){
+            if(commentValidation() === false){
                 event.preventDefault()
             }
+
             $('#comment-textarea').on('keyup', function () {
                 commentValidation();
             });
 
+            if(commentValidation() === true){
+                $('#commentForm').trigger('submit', [true]);
+            }
+        });
+
+        $('#commentForm').submit(function (event) {
+            let form = $(this)
+            let widgetId = form.find('.g-recaptcha').data('widget-id');
+            if (typeof widgetId === 'undefined') {
+                return;
+            }
             if (typeof grecaptcha == 'object') {
-                if (!grecaptcha.getResponse()) {
+                if (!grecaptcha.getResponse(widgetId)) {
                     event.preventDefault();
-                    grecaptcha.execute();
+                    grecaptcha.execute(widgetId);
                 }
             } else {
                 event.preventDefault();
             }
-            $('#submitButton').prop("disabled", true);
 
-        });
-
-        $('#commentForm #submitButton').mousedown(function (event) {
-            var field = this;
-            if (submitAmount === 0) {
-                submitAmount++;
-                field.disabled = true;
-/*                setTimeout(function enable() {
-                    submitAmount = 0;
-                    field.disabled = false;
-                }, 6000);*/
-                $('#commentForm').trigger('submit');
-            }
-        });
+        })
 
         function commentValidation() {
             let textareaElement = $('#comment-textarea');
-            var commentLength = textareaElement.val().length;
+            let commentLength = textareaElement.val().length;
             let validComment = ( minCharacters <= commentLength && commentLength <= maxCharacters ) || commentLength === 0;
             $('#submitButton').attr('disabled', !validComment)
             $('#error-message-too-short').toggleClass('d-none', validComment)
@@ -93,20 +78,33 @@ $( document ).ready(function() {
         }
 
         function checkCommentLength(){
-            maxLabel = document.getElementById('maxLabel').getAttribute('data-tr-label')
-            charLabel = document.getElementById('charLabel').getAttribute('data-tr-label')
-            var currentLength = maxCharacters -  $('#comment-textarea').val().length
+            let maxLabel = document.getElementById('maxLabel').getAttribute('data-tr-label')
+            let charLabel = document.getElementById('charLabel').getAttribute('data-tr-label')
+            let currentLength = maxCharacters -  $('#comment-textarea').val().length
             if(currentLength >= 0){
                 document.getElementById('limitLabel').innerHTML = maxLabel + currentLength + charLabel
             }
-            /*if(currentLength <= 0){
-                $('#comment-textarea').attr("maxlength",0);
-            }*/
         }
-
     }
+
 });
+// Render reCAPTCHA
+function onloadCallback () {
+    $(".g-recaptcha").each(function (index) {
+        $(this).data('widget-id', index);
+        let key = $(this).attr('data-sitekey');
+        grecaptcha.render($(this).attr('id'), {
+            'sitekey': key,
+            'callback': onCompleted
+        });
+    });
+};
 function onCompleted() {
     $('#submitButton').prop("disabled", true);
-    $('#commentForm').trigger('submit', [true]);
+    if (submitAmount === 0) {
+        $('#commentForm').trigger('submit', [true]);
+    }
+    submitAmount++;
+    grecaptcha.reset();
+
 }
