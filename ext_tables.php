0005c70ca8172f1241655052d611be0f4251b98b
Abderrazak Tgiti <tigitiabderrazak@gmail.com>
2023-09-22 13:22:47 +0100

    update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -21,7 +21,7 @@ call_user_func(
             'Add comments section in page'
         );
 
-        ExtensionUtility::registerModule(
+/*        ExtensionUtility::registerModule(
             'QcComments',
             'web',
             'admin',
@@ -35,7 +35,7 @@ call_user_func(
                 'icon' => 'EXT:qc_comments/Resources/Public/Icons/qc_comments.svg',
                 'labels' => 'LLL:EXT:qc_comments/Resources/Private/Language/locallang.xlf:qc_comments',
             ]
-        );
+        );*/
         ExtensionManagementUtility::addStaticFile('qc_comments', 'Configuration/TypoScript', 'Module used to manage FE users comments on pages');
     }
 );
